142ad3c21a2e27186fcb8566f1b352b77411671d
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2025-03-12 17:18:30 +0100

    run_all_tests.bash: adjust file to work on many systems
    Signed-off-by: Kamil Pokornicki <kamilpokornicki11@gmail.com>

--- run_all_tests.bash
+++ run_all_tests.bash
@@ -9,16 +9,32 @@ TEST_SETS=(
 
 BASE_DIR="test_sets"
 LOGS_DIR="logs"
-mkdir -p ${LOGS_DIR}
-.venv/Scripts/activate 
+
+mkdir -p "${LOGS_DIR}"
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    VENV_ACTIVATE=".venv/Scripts/activate"
+else
+    VENV_ACTIVATE=".venv/bin/activate"
+fi
+
+if [ -f "$VENV_ACTIVATE" ]; then
+    source "$VENV_ACTIVATE"
+else
+    echo "Error: Virtual environment activation script not found!"
+    exit 1
+fi
+
+pip install -r requirements.txt
+rfbrowser init
 
 for TEST_SET in "${TEST_SETS[@]}"; do
     TEST_PATH="${BASE_DIR}/${TEST_SET}/tests.robot"
-    LOG_FILE="${LOGS_DIR}/${TEST_SET}.html"
+    TEST_LOG_DIR="${LOGS_DIR}/${TEST_SET}"
+    mkdir -p "${TEST_LOG_DIR}"
 
     echo "Running tests for: ${TEST_SET}"
-
-    python -m robot --log ${LOG_FILE} ${TEST_PATH}
+    robot --outputdir "${TEST_LOG_DIR}" "${TEST_PATH}"
 
     if [ $? -ne 0 ]; then
         echo "Tests for ${TEST_SET} failed!"
