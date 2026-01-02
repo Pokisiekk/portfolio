615513730172eadde0f38ab789b6eb1b2d029111
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2026-01-02 10:24:35 +0100

    run_youtube_tests.ps1: add retries

--- run_youtube_tests.ps1
+++ run_youtube_tests.ps1
@@ -20,6 +20,16 @@ pip install -r requirements.txt
 rfbrowser init
 
 robot --outputdir $logsDir --output original.xml $testPath
+if ($LASTEXITCODE -ne 0) {
+    robot --outputdir $logsDir --rerunfailed (Join-Path $logsDir "original.xml") --output rerun1.xml $testPath
+    if ($LASTEXITCODE -ne 0) {
+        robot --outputdir $logsDir --rerunfailed (Join-Path $logsDir "rerun1.xml") --output rerun2.xml $testPath
+        rebot --outputdir $logsDir --merge (Join-Path $logsDir "original.xml") (Join-Path $logsDir "rerun1.xml") (Join-Path $logsDir "rerun2.xml")
+    }
+    else {
+        rebot --outputdir $logsDir --merge (Join-Path $logsDir "original.xml") (Join-Path $logsDir "rerun1.xml")
+    }
+}
 
 Write-Host "Youtube tests completed."
 Remove-Item -Recurse -Force $tempVenv
