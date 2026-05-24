6dda176d8959581486452655f524914db6f27112
Kamil Pokornicki <kamilpokornicki11@gmail.com>
2026-05-24 16:16:51 +0200

    run_orangeHRM_tests.bash: add file

---
+++ run_orangeHRM_tests.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TEST_PATH="test_sets/orangeHRM/"
+LOGS_DIR="logs/orangeHRM"
+TEMP_VENV=$(mktemp -d)
+
+mkdir -p "${LOGS_DIR}"
+python -m venv "$TEMP_VENV"
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    VENV_ACTIVATE="$TEMP_VENV/Scripts/activate"
+else
+    VENV_ACTIVATE="$TEMP_VENV/bin/activate"
+fi
+
+if [ -f "$VENV_ACTIVATE" ]; then
+    source "$VENV_ACTIVATE"
+else
+    echo "Error: Virtual environment activation script not found!"
+    exit 1
+fi
+
+pip install -r requirements.txt
+rfbrowser init
+
+robot --outputdir "${LOGS_DIR}" --output original.xml "${TEST_PATH}"
+if [ $? -ne 0 ]; then
+    robot --outputdir "${LOGS_DIR}" --rerunfailed "${LOGS_DIR}/original.xml" --output rerun1.xml "${TEST_PATH}"
+    if [ $? -ne 0 ]; then
+        robot --outputdir "${LOGS_DIR}" --rerunfailed "${LOGS_DIR}/rerun1.xml" --output rerun2.xml "${TEST_PATH}"
+        rebot --outputdir "${LOGS_DIR}" --merge "${LOGS_DIR}/original.xml" "${LOGS_DIR}/rerun1.xml" "${LOGS_DIR}/rerun2.xml"
+    else
+        rebot --outputdir "${LOGS_DIR}" --merge "${LOGS_DIR}/original.xml" "${LOGS_DIR}/rerun1.xml"
+    fi
+fi
+
+echo "OrangeHRM tests completed."
+
+deactivate
+rm -rf "$TEMP_VENV"
